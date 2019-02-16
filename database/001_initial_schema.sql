436867d9f9d709c1b3230c6eb3518d5698f37237
Yngve Molnes <yngve.molnes@gmail.com>
2019-02-16 15:04:07 +0100

    Syntax corrections and bash-script to create database

--- database/001_initial_schema.sql
+++ database/001_initial_schema.sql
@@ -2,20 +2,19 @@ CREATE TABLE IF NOT EXISTS public.net_json_graph (
   -- Interatively store the network graph we fetch
   -- from a tactical router. Could provide useful for
   -- historic purposes
-  net_json_graph_id SERIAL PRIMARY KEY,
+  net_json_graph_id SERIAL PRIMARY KEY NOT NULL,
   -- we might not even need a key here
   content JSONB NOT NULL,
   created TIMESTAMPTZ NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.device (
-  device_id SERIAL PRIMARY KEY 
+CREATE TABLE IF NOT EXISTS public.device(
+  device_id SERIAL PRIMARY KEY NOT NULL
   -- what does a device consist of?
 );
 
 CREATE TABLE IF NOT EXISTS public.adaption (
   adaption_id SERIAL PRIMARY KEY NOT NULL
-  
   -- what does an adaption consist of?
 );
 
@@ -24,9 +23,9 @@ CREATE TABLE IF NOT EXISTS public.system_log (
   -- for adaptions made in the system
   system_log_id SERIAL PRIMARY KEY NOT NULL,
   device_id INTEGER NOT NULL REFERENCES device(device_id),
-  adaption_id INTEGER REFERENCES device(adaption_id),
+  adaption_id INTEGER REFERENCES adaption(adaption_id),
   description TEXT NOT NULL,
-  created TIMESTAMPTZ NOT NULL,
+  created TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.position_log (
---
+++ setup_db.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Dropping db if exists (…)"
+dropdb --if-exists netjson_dev
+echo "Creating database (…)"
+createdb netjson_dev 
+# $? is a magic bash variable assigned
+# to the exit code of the last command
+if [ $? -eq 0 ]; then
+  echo "Database created."
+  echo "Creating database structure (…)"
+  psql -d netjson_dev -f database/001_initial_schema.sql
+else
+  echo "Creating database structure failed"
+  exit 1
+fi
