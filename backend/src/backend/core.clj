aa2dcf367fcc11dc8ec9508174691c5b89304047
Yngve Molnes <yngve.molnes@gmail.com>
2019-02-18 10:27:51 +0100

    Fix autogenerating docs with docstrings

--- backend/src/backend/core.clj
+++ backend/src/backend/core.clj
@@ -1,11 +1,15 @@
 (ns backend.core
   (:require [org.httpkit.server :refer [run-server]]))
 
-(defn app [req]
+(defn app 
+  "Says hello, world"
+  [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (str "Hello, World")})
 
-(defn -main [& args]
+(defn -main 
+  "Starts a http-server"
+  [& args]
   (run-server app {:port 8080})
   (println "Server started on port 8080"))
