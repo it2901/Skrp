3c221ea4fe286e35f6c122006b99f7b177e61e19
skykanin <3789764+skykanin@users.noreply.github.com>
2019-02-22 16:29:27 +0100

    Fixed core dummy test

--- backend/test/backend/core_test.clj
+++ backend/test/backend/core_test.clj
@@ -1,7 +1,6 @@
 (ns backend.core-test
-  (:require [clojure.test :refer :all]
-            [backend.core :refer :all]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
