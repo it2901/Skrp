6e5cdd10b090bde304401dd477a7681c3645037a
skykanin <3789764+skykanin@users.noreply.github.com>
2019-02-22 13:55:27 +0100

    Added test file

--- backend/test/backend/routes/netjson_test.clj
+++ backend/test/backend/routes/netjson_test.clj
@@ -1,2 +1,6 @@
 (ns backend.routes.netjson-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as spec]))
+
+(spec/def :networkgraph
+  (spec/keys :type ))
