d53f7a6dcaa90efbfae056aaa3886931c562c51c
Endre Ulberg <emulberg@stud.ntnu.no>
2019-02-25 11:43:32 +0100

    Update Contact.js

--- frontend/src/components/test/Contact.js
+++ frontend/src/components/test/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
       <div>
         <h2>GOT QUESTIONS?</h2>
         <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
+        our <a href="https://freak.no/forum/showthread.php?s=3d0bf76ee3d0f65a5725cd33f7ce0770&t=86181">forums</a>.
         </p>
       </div>
     );
