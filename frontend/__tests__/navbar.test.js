8166831c98ee1d8da5f38b4b0687db5cffbf9a1c
Thomas Almestad <thomasdasd@gmail.com>
2019-03-04 13:07:14 +0100

    set up basic snapshot testing

---
+++ frontend/__tests__/navbar.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer'
+import Navbar from '../src/components/navbar'
+
+it('renders correctly', ()=>{
+    const tree=renderer
+        .create(<Navbar></Navbar>)
+        .toJSON();
+    expect(tree).toMatchSnapshot()
+})
