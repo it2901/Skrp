b22848747b861a1285745f1cd5ab4f1e93ff9aa1
Joakim Fremstad <joa.fre@gmail.com>
2019-03-04 11:39:58 +0100

    pick f8e68b1 Add a seed to the database

--- database/README.md
+++ database/README.md
@@ -3,3 +3,6 @@
 * `docker build -t netjson_postgres .`
 * `mkdir -p persistent_db`
 * `docker run --rm --name pg-docker -e POSTGRES_DB=netjson_dev -e POSTGRES_PASSWORD=password -d -p 5432:5432 -v ${PWD}/persistent_db:/var/lib/postgresql/data netjson_postgres`
+
+## Dumping database
+* `pg_dump -a -h localhost -U postgres netjson_dev > datadump.sql`
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import NavBar from "./components/navbar"
 import NodeGraph from "./components/NodeGraph"
 import About from "./components/about"
 import errorcomponent from "./components/errorcomponent"
+import tweakInput from "./components/tweakInput"
+import styled from "styled-components"
+import {Helmet} from 'react-helmet';
 
 import {
   Route,
@@ -12,23 +15,37 @@ import {
 import "./components/styles/navbar.css"
 import "./components/styles/hashrouter.css"
 
+const Div = styled.div`
+height: 100%;
+width:100%;
+color:white;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        {/* The Helmet gives a uniform color to the whole page. */}
+      <Helmet>
+      <style>{'body { background-color: black; }'}</style>
+    </Helmet>
+    {/* This is the acctual components and navbar*/}
+
+      <Div>
         <NavBar/>
           <HashRouter>
             <div className="SPA">
               <Switch>
                 <Route path="/home" component={About}/>
+                <Route path="/tweakinput" component={tweakInput}/>
                 <Route path="/nodegraph" component={NodeGraph}/>
                 <Route path="/about" component={About}/>
                 <Route component={errorcomponent} />
               </Switch>
             </div>  
         </HashRouter>
-      </div>
+        </Div>
+        </div>
     );
   }
 }
