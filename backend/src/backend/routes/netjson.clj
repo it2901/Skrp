5f911d94f750c99929aa40536b35c9b45524b410
Fredrik Svendsen <fredrsve@stud.ntnu.no>
2019-03-25 20:19:39 +0100

    now setting right adaption_id in system_log when an adaption is made

--- backend/src/backend/routes/netjson.clj
+++ backend/src/backend/routes/netjson.clj
@@ -17,7 +17,8 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [clojure.java.io :as io]
-            [backend.logging :refer [get-syslog, set-device-id, insert-adaption]]
+            [backend.logging :refer [get-syslog, set-device-id, insert-adaption, insert-syslog,]]
+                                     ;;testreturn, test-parse]]
             [ring.middleware.json :refer [wrap-json-response]]))
 
 (defn index-handler
@@ -67,51 +68,79 @@
                                     (params "dateto")))
     :else (error-handler-rep 400 "Invalid query")))
 
-#_(defn adaption-handler
-      ""
-      [{params :query-params :as req}]
-      (cond
-        (empty? params) (error-handler-rep 400 "Make at least one adaption.")
-        (contains? params "adaption1") (update adaption 1)
-        (contains? params "adaption2") (up)
-        :else (error-handler-rep 400 "Invalid adaptions")))
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s)))
+
+;;TODO replace dummy-adaptions
+;;TODO get/set real device_id
+;;TODO return something else
+;;TODO error-handling
+(defn adaption-handler
+  [{:keys [adaption1 adaption2 adaption3 adaption4 adaption5 device_id description]}]
+  (let [adaption_id (parse-int (apply str (insert-adaption {:adaption1 adaption1
+                                                            :adaption2 adaption2
+                                                            :adaption3 adaption3
+                                                            :adaption4 adaption4
+                                                            :adaption5 adaption5})))]
+    (insert-syslog {:device_id 990
+                    :adaption_id adaption_id
+                    :description description})))
+
 
 ;;TODO get real device id (mac/ip), + adaption id
 (defn adaption-check
   "Insert adaption-data"
   [{params :query-params :as req}]
-
   (def device-id (rand-int 1000))
   (set-device-id device-id)
-  (def adaption-id (rand-int 1000))
-  (and
-    (contains? params "adaption1")
-    (contains? params "adaption2")
-    (contains? params "adaption3")
-    (contains? params "adaption4")
-    (contains? params "adaption5")) (adaption-handler
-                                      (params {:adaption1 params "adaption1",
-                                               :adaption2 params "adaption2",
-                                               :adaption3 params "adaption3",
-                                               :adaption4 params "adaption4",
-                                               :adaption5 params "adaption5",
-                                               :device-id device-id,
-                                               :adaption-id adaption-id,
-                                               :description "desctest"}))
-  :else (error-handler-rep 400 "Invalid adaption"))
+  (cond
+    (and
+      (contains? params "adaption1")
+      (contains? params "adaption2")
+      (contains? params "adaption3")
+      (contains? params "adaption4")
+      (contains? params "adaption5")
+      (contains? params "description")) (adaption-handler {:adaption1 (parse-int (params "adaption1"))
+                                                           :adaption2 (parse-int (params "adaption2"))
+                                                           :adaption3 (parse-int (params "adaption3"))
+                                                           :adaption4 (parse-int (params "adaption4"))
+                                                           :adaption5 (parse-int (params "adaption5"))
+                                                           :description (params "description")})
+                                                           ;;:device_id device-id})
+    :else (error-handler-rep 400 "Invalid adaption")))
 
-(defn adaption-handler
-  [{:keys [adaption1, adaption2, adaption3, adaption4, adaption5, device-id, adaption-id, description]}]
-  (insert-adaption
-    ({:adaption1 "adaption1",
-      :adaption2 "adaption2",
-      :adaption3 "adaption3",
-      :adaption4 "adaption4",
-      :adaption5 "adaption5"}))
-  (insert-syslog ({:device_id device-id,
-                   :adaption_id adaption-id,
-                   :description description})))
+;;tester fn btw
+;; XTODO  param to int?
+;; TODO return
+;; TODO wrap?
+#_(defn test-set-device-and-adaption-id
+    [{params :query-params :as req}]
+    (contains? params "n") ((def deviceid (rand-int 1000))
+                            (def adaptionid (rand-int 10000))
+                            ;(set-device-id deviceid)
+                            ;;(def adaptionint (parse-int (params "n")))
+                            ;;(def z (params "n"))
+                            ;;(parse-int (params "n"))
+                            ;;(println "TYPE: "(type (params "n")))
+                            ;;(println "TYPE: " (type adaptionint))
+                            (insert-adaption {:adaption_id (parse-int (params "n"))
+                                              :adaption1 1
+                                              :adaption2 2
+                                              :adaption3 3
+                                              :adaption4 4
+                                              :adaption5 5})))
 
+
+#_(defn testfunc
+    [{params :query-params :as req}]
+    (contains? params "n") (insert-adaption {:adaption_id (parse-int (params "n"))}))
+
+#_(defn returntest [_]
+    {:body (str (parse-int (apply str (insert-adaption {:adaption1 1
+                                                        :adaption2 2
+                                                        :adaption3 3
+                                                        :adaption4 4
+                                                        :adaption5 5}))))})
 
 (defroutes app-routes
   "Defines all the routes and their respective route handlers"
@@ -119,6 +148,8 @@
   (GET "/networkgraph" [] (wrap-json-response dummy-data-handler))
   (GET "/syslog" request (wrap-json-response syslog-handler))
   (GET "/adaptiontest" request adaption-check)
+  #_(GET "/testid" request (wrap-json-response test-set-device-and-adaption-id))
+  #_(GET "/returntest" [] returntest)
   (route/not-found (wrap-json-response
                     (error-handler-rep 404
                                        "Could not find route"))))
