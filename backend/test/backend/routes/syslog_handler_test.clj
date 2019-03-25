5d007353f93cbb9f8016c7470ad156938eace3df
skykanin <3789764+skykanin@users.noreply.github.com>
2019-03-25 23:25:17 +0100

    Added test for syslog route

---
+++ backend/test/backend/routes/syslog_handler_test.clj
@@ -0,0 +1,43 @@
+(ns backend.routes.syslog-handler-test
+  (:require [clojure.data.json :refer [read-str]]
+            [clojure.test :refer :all]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.mock.request :as mock]))
+
+(def mock-data {:status 200
+                :headers {"Content-Type" "application/json"}
+                :body
+                [{"system_log_id" 3
+                  "device_id" 111222
+                  "adaption_id" 1
+                  "description" "This is a meme test"
+                  "created" "2019-03-22T14:02:12Z"}
+                 {"system_log_id" 4
+                  "device_id" 888999
+                  "adaption_id" 2
+                  "description" "This is a test boissssssssssssss"
+                  "created" "2019-03-22T14:15:58Z"}]})
+
+(defn mock-syslog
+  ([] nil)
+  ([_] nil)
+  ([_ _] nil))
+
+(defn run-mock [req-str]
+  (with-redefs
+   [backend.routes.netjson/syslog-check (fn [_] mock-data)
+    backend.logging/get-syslog mock-syslog
+    (as-> req-str s
+      (mock/request :get s)
+      ((wrap-params backend.routes.netjson/app-routes) s)
+      (update s :body read-str))]))
+
+(deftest syslog-test
+  (testing "system log query"
+    (let [bad-rep (run-mock "/syslog?dasdsa=2019-01-01")
+          good-rep (run-mock "/syslog")]
+      (is (= {:status 400
+              :headers {"Content-Type" "application/json"}
+              :body {"Error" "Invalid query"}}
+             bad-rep))
+      (is (= mock-data good-rep)))))
