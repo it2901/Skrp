d450d85bfea937c8cb66707fdc29e1fb40334103
emulberg <emulberg@stud.ntnu.no>
2019-04-10 19:30:41 +0200

    cleaned the data that is exposed on the API

--- mocker/mocker.js
+++ mocker/mocker.js
@@ -6,13 +6,13 @@ const port = 3001;
 let x = 1;
 app.get('/',(req,res) => res.send('Hello world'))
 let geolocations = require('./Schemes/geolocations.json')  
-const geolocationsEnd = jsf.generate(geolocations);
+const geolocationsEnd = JSON.stringify(jsf.generate(geolocations).collection[0]);
 let networkdomain = require('./Schemes/networkdomain.json')  
-const networkdomainEnd = jsf.generate(networkdomain);
+const networkdomainEnd = JSON.stringify(jsf.generate(networkdomain).collection[0]);
 let networkgraph = require('./Schemes/networkgraph.json')  
-const networkgraphEnd = jsf.generate(networkgraph);
+const networkgraphEnd = JSON.stringify(jsf.generate(networkgraph).collection[0]);
 let networkroutes = require('./Schemes/networkroutes.json')
-const networkroutesEnd = jsf.generate(networkroutes);
+const networkroutesEnd = JSON.stringify(jsf.generate(networkroutes).collection[0]);
 
 app.get('/netrou',(req,res) => res.send(networkroutesEnd))
 app.get('/geoloc',(req,res) => res.send(geolocationsEnd))
