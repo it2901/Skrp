e510666dfb17060d474323dc2bcb59f912c30385
Thomas Almestad <thomasdasd@gmail.com>
2019-04-11 23:56:25 +0200

    input shows error

--- frontend/src/components/log.js
+++ frontend/src/components/log.js
@@ -239,7 +239,7 @@ class Log extends Component {
     return (
       <div>
         <Form.Input {...props}
-
+          error={!this.state.canFilter && !this.state[name]}
           icon={
             <Icon link name='close' onClick={clear} />
           }
