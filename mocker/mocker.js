8ce9d415a9a60fe8c71cdd6da6a9f59a8859894c
emulberg <emulberg@stud.ntnu.no>
2019-04-11 19:56:50 +0200

    made the mocker update the JSONs at the endpoints every 5 seconds

--- mocker/mocker.js
+++ mocker/mocker.js
@@ -3,17 +3,19 @@ const jsf =  require('json-schema-faker');
 const app = express()
 const port = 3001;
 
-let x = 1;
-app.get('/',(req,res) => res.send('Hello world'))
-let geolocations = require('./Schemes/geolocations.json')  
-const geolocationsEnd = jsf.generate(geolocations).collection[0];
-let networkdomain = require('./Schemes/networkdomain.json')  
-const networkdomainEnd = jsf.generate(networkdomain).collection[0];
-let networkgraph = require('./Schemes/networkgraph.json')  
-const networkgraphEnd = jsf.generate(networkgraph).collection[0];
-let networkroutes = require('./Schemes/networkroutes.json')
-const networkroutesEnd = jsf.generate(networkroutes).collection[0];
 
+setInterval(() => {
+    let geolocations = require('./Schemes/geolocations.json')  
+    let networkdomain = require('./Schemes/networkdomain.json')  
+    let networkgraph = require('./Schemes/networkgraph.json')  
+    let networkroutes = require('./Schemes/networkroutes.json')
+     geolocationsEnd = jsf.generate(geolocations).collection[0];
+     networkdomainEnd = jsf.generate(networkdomain).collection[0];
+     networkgraphEnd = jsf.generate(networkgraph).collection[0];
+     networkroutesEnd = jsf.generate(networkroutes).collection[0];
+}, 5000);
+
+app.get('/',(req,res) => res.send('Hello world'))
 app.get('/netrou',(req,res) => res.send(networkroutesEnd))
 app.get('/geoloc',(req,res) => res.send(geolocationsEnd))
 app.get('/netdom',(req,res) => res.send(networkdomainEnd))
