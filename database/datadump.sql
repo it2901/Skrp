10cf903a5a9f5f6a3fb6873dfe675d03d222a350
Joakim Fremstad <joa.fre@gmail.com>
2019-04-12 09:59:31 +0200

    New datadump

--- database/datadump.sql
+++ database/datadump.sql
@@ -16,14 +16,12 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: adaption; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: device; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.adaption (adaption_id) FROM stdin;
-2
+COPY public.device (device_id) FROM stdin;
 1
-5
-4
+2
 3
 \.
 
@@ -32,22 +30,19 @@ COPY public.adaption (adaption_id) FROM stdin;
 -- Data for Name: config; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.config (conf_id, p1, p2, p3, p4, p5, p6) FROM stdin;
-1	1	2	3	4	5	6
-2	2	4	6	8	10	12
+COPY public.config (config_id, device_id, config, created) FROM stdin;
+1	1	{"id": 1, "data": "aaa"}	2019-04-12 07:39:11.230053+00
+2	2	{"id": 3, "data": "aaeouaoeuaa"}	2019-04-12 07:39:21.996719+00
 \.
 
 
 --
--- Data for Name: device; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: adaption; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.device (device_id) FROM stdin;
-1
-2
-3
-4
-5
+COPY public.adaption (adaption_id, adaption_type, adaption_description, config_id) FROM stdin;
+1	compress	Make the things smaller	1
+2	switch prot	change protocol	2
 \.
 
 
@@ -72,10 +67,8 @@ COPY public.position_log (device_id, lat, lng, created) FROM stdin;
 --
 
 COPY public.system_log (system_log_id, device_id, adaption_id, description, created) FROM stdin;
-4	1	2	This is database	2012-02-09 10:38:01+00
-1	1	1	Compress	2019-03-04 09:46:06.693675+00
-2	2	3	Switch protocol	2019-03-04 09:46:31.69302+00
-3	3	3	Disable compression	2019-03-04 09:47:03.291785+00
+1	1	1	Disable compression	2019-04-12 07:55:56.854202+00
+2	2	2	Switching protocol	2019-04-12 07:57:33.376585+00
 \.
 
 
@@ -87,10 +80,10 @@ SELECT pg_catalog.setval('public.adaption_adaption_id_seq', 1, false);
 
 
 --
--- Name: config_conf_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: config_config_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.config_conf_id_seq', 1, false);
+SELECT pg_catalog.setval('public.config_config_id_seq', 1, false);
 
 
 --
@@ -111,7 +104,7 @@ SELECT pg_catalog.setval('public.net_json_graph_net_json_graph_id_seq', 1, false
 -- Name: system_log_system_log_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.system_log_system_log_id_seq', 4, true);
+SELECT pg_catalog.setval('public.system_log_system_log_id_seq', 1, false);
 
 
 --
