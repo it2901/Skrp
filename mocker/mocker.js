f436f8ede34a6f714154a479f72fe5b6a95b4aec
Endre Ulberg <endremau@gmail.com>
2019-04-14 12:02:50 +0200

    added new endpoint for geolocation and nodegraph connected together

--- mocker/mocker.js
+++ mocker/mocker.js
@@ -2,20 +2,23 @@ const express = require('express');
 const jsf =  require('json-schema-faker');
 const app = express()
 const port = 3001;
-
+const cwd = `${__dirname}/Schemes`;
 
 setInterval(() => {
+     mapnodes = require('./Schemes/mapnodes.json')  
      geolocations = require('./Schemes/geolocations.json')  
      networkdomain = require('./Schemes/networkdomain.json')  
      networkgraph = require('./Schemes/networkgraph.json')  
      networkroutes = require('./Schemes/networkroutes.json')
+     mapnodesEnd = jsf.generate(mapnodes).collection[0];
      geolocationsEnd = jsf.generate(geolocations).collection[0];
      networkdomainEnd = jsf.generate(networkdomain).collection[0];
-     networkgraphEnd = jsf.generate(networkgraph).collection[0];
+     networkgraphEnd = jsf.generate(networkgraph,cwd).collection[0];
      networkroutesEnd = jsf.generate(networkroutes).collection[0];
 }, 5000);
 
 app.get('/',(req,res) => res.send('Hello world'))
+app.get('/mapnod',(req,res) => res.send(mapnodesEnd))
 app.get('/netrou',(req,res) => res.send(networkroutesEnd))
 app.get('/geoloc',(req,res) => res.send(geolocationsEnd))
 app.get('/netdom',(req,res) => res.send(networkdomainEnd))
