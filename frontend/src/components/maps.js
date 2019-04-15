8e33dd239e393ffe9ca74f82ec567c17a6eb5f85
Endre Ulberg <endremau@gmail.com>
2019-04-15 12:01:19 +0200

    Added the links to the page | pliz review the code here

--- frontend/src/components/maps.js
+++ frontend/src/components/maps.js
@@ -10,13 +10,16 @@ export default class Maps extends React.Component {
         lat: 52.3,
         lng: 13.5,
         zoom: 18,
+        links:[{
+            cost:123,
+            target:"123.123.123.11",
+            source:"123.123.123.12"
+        }],
         nodes:
           [{"123.123.123.11": {
-          lat: 52.3,
-          lng: 13.5}},
+          pos: [52.3,13.5]}},
           {"123.123.123.12": {
-            lat: 52.2,
-            lng: 13.4}}
+            pos:[52.2,13.4]}}
           ]
     }}
 
@@ -27,20 +30,43 @@ export default class Maps extends React.Component {
         let nodes = stateToBe["nodes"]
         let links = stateToBe["links"]
         let keys = nodes.map(node => {return node["id"]})
-        let x = 0
         this.setState({
-            nodes:nodes.map(node =>{
+            ["nodes"]:nodes.map(node =>{
                 return {[node["id"]] :{
-                  lat:node["Location"]["Position"]["Latitude"],
-                  lng:node["Location"]["Position"]["Longitude"],
+                  pos:[node["Location"]["Position"]["Latitude"],node["Location"]["Position"]["Longitude"]],
                   neighbours:0,
                   time: node["Location"]["Time"]
                 }}
 
             })
+        
         })
-        console.log(this.state)
-          
+        
+       this.setState({
+            links:links.map(link =>{
+                return{
+                cost:link["cost"],
+                source:link["source"],
+                target:link["target"]
+                }
+
+
+            })
+        })
+      }
+
+      addNeighbour(id){}
+
+
+      findLatLng(id){
+        let nodes = this.state.nodes
+        let pos = nodes.map(node => {
+            let key = Object.keys(node)[0]
+            if (id == key){
+                 return node[key]["pos"] 
+            } 
+        });
+        return pos
       }
 
     updateDimensions() {
@@ -59,20 +85,36 @@ export default class Maps extends React.Component {
             [52.3, 13.4],
             [52.2, 13.3]
         ];
-       
+  
         let nodes = this.state.nodes.map(obj => {
             let key = Object.keys(obj)
           let node = obj[key]
-          let x = node["lng"]
-          let y = node["lat"]
+          let pos = node["pos"]
           return (
 
-            <Marker position={[y,x]}>
+            <Marker position={pos}>
                 <Popup>{key}<br />Easily customizable.</Popup>
-                <Circle name={key}center={[y,x]} radius={200} />
+                <Circle name={key}center={pos} radius={200} />
             </Marker>)
         })
 
+        let links = this.state.links.map(link => {
+            let source = this.findLatLng(link["source"])
+            let target = this.findLatLng(link["target"])
+            let cost = link["cost"]
+            let colors = ["green","lime","GreenYellow ","yellow","orange","OrangeRed ","red","Crimson","DarkRed ","black"]
+            let targeter = Math.round(cost/1000000)
+            //console.table([this.findLatLng(source),this.findLatLng(target)])
+            //Only works for basecase (aka no fetch from mocker 
+            //let pos = [this.findLatLng(source)[1],this.findLatLng(target)[0]]
+            let cleanedSource = source.filter((y) => { if (y != undefined) {return y}})
+            let cleanedTarget = target.filter((y) => { if (y != undefined) {return y}})
+            let pos = [cleanedSource,cleanedTarget]
+            console.log(pos)
+            return (
+                <Polyline color={colors[targeter]} positions={pos} />
+            )
+        })
         
         const position = [this.state.lat, this.state.lng]
         return (
@@ -89,7 +131,7 @@ export default class Maps extends React.Component {
       }}
     />
     {nodes}
-    <Polyline color="lime" positions={latlngs}/>
+    {links}
   </FeatureGroup>
             </Map>
         )
