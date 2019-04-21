57c7c9db393ce5e1ae2677151ad5d0c2ccd15552
Endre Ulberg <endremau@gmail.com>
2019-04-21 11:57:34 +0200

    made errorcomponent 100% the size of the screen

--- frontend/src/components/errorcomponent.js
+++ frontend/src/components/errorcomponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import image from '../assets/404.gif'
 
+const size = {
+  width: "100%"
+  
+}
+
 class Errorcomponent extends Component {
   render () {
     return (
-      <div>
-        <img src={image} alt="Logo" />
+      <div style={size}>
+        <img  style={size} src={image} alt="Logo" />
       </div>
     )
   }
