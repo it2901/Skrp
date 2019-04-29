5d6250a3e09d21fb4907b24353fb26e78469a1d5
emulberg <emulberg@stud.ntnu.no>
2019-04-29 17:30:31 +0200

    added new schemas and API ends to the mocker

--- mocker/mocker.js
+++ mocker/mocker.js
@@ -4,12 +4,21 @@ const app = express()
 const port = 3001;
 const cwd = `${__dirname}/Schemes`;
 
+
+
+
 setInterval(() => {
+     devmon = require('./Schemes/deviceconfiguration.json')
+     netcoll = require('./Schemes/networkcollection.json')  
+     devconf = require('./Schemes/deviceconfiguration.json')
      mapnodes = require('./Schemes/mapnodes.json')  
      geolocations = require('./Schemes/geolocations.json')  
      networkdomain = require('./Schemes/networkdomain.json')  
      networkgraph = require('./Schemes/networkgraph.json')  
      networkroutes = require('./Schemes/networkroutes.json')
+     devmonEnd = jsf.generate(devmon)
+     devconfEnd = jsf.generate(devconf)
+     netcollEnd = jsf.generate(netcoll).collection[0];
      mapnodesEnd = jsf.generate(mapnodes).collection[0];
      geolocationsEnd = jsf.generate(geolocations).collection[0];
      networkdomainEnd = jsf.generate(networkdomain).collection[0];
@@ -17,7 +26,9 @@ setInterval(() => {
      networkroutesEnd = jsf.generate(networkroutes).collection[0];
 }, 5000);
 
-app.get('/',(req,res) => res.send('Hello world'))
+app.get('/devmon',(req,res) => res.send(devmonEnd))
+app.get('/devcon',(req,res) => res.send(devconfEnd))
+app.get('/netcol',(req,res) => res.send(netcollEnd))
 app.get('/mapnod',(req,res) => res.send(mapnodesEnd))
 app.get('/netrou',(req,res) => res.send(networkroutesEnd))
 app.get('/geoloc',(req,res) => res.send(geolocationsEnd))
