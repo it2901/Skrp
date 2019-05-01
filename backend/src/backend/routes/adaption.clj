cc29c75a586f887dde24262c56c7aa5aea82fd59
skykanin <3789764+skykanin@users.noreply.github.com>
2019-05-01 21:20:22 +0200

    Fixed formatting

--- backend/src/backend/routes/adaption.clj
+++ backend/src/backend/routes/adaption.clj
@@ -6,24 +6,24 @@
 (defn command-builder
   "Builds the response map"
   [{:keys [eq-proto? protos device-id]}]
-  {:name "Adaption Command"
-   :type "Change Protocol"
-   :device-id device-id
-   :options {:protocol (if eq-proto?
-                         (:current protos)
-                         (:new protos))
-             :keep-alive-period 10
-             :max-retries 3
-             :waiting-time 10}})
-
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body {:name "Adaption Command"
+          :type "Change Protocol"
+          :device-id device-id
+          :options {:protocol (if eq-proto?
+                                (:current protos)
+                                (:new protos))
+                    :keep-alive-period 10
+                    :max-retries 3
+                    :waiting-time 10}}})
 
 (defn adaption-request-handler
   "HTTP GET handler for requesting network adaptions. This endpoint
   will log the data it recieves and respond with a suitable adaption.
   Query params are collection and device-id"
   [{params :query-params :as req}]
-  (println (type (params "collection")))
-  (let [netcoll (read-str (str (params "collection")) :key-fn keyword)
+  (let [netcoll (read-str (params "collection") :key-fn keyword)
         get-proto (fn [nc]
                     (as-> nc nc
                       (filter #(= "NetworkGraph" (:type %)) nc)
@@ -32,10 +32,11 @@
     (if netcoll
       (let [[_ data _] (get-network-collection :latest)
             db-proto (get-proto (:collection data))
-            req-proto (get-proto netcoll)]
+            req-proto (get-proto (:collection netcoll))]
         (command-builder
          {:eq-proto? (= req-proto db-proto)
           :protos {:current db-proto
                    :new req-proto}
-          :device-id (params "device-id")}))
+          :device-id (Integer/parseInt (params "device-id"))}))
+
       (error-handler-rep 400 "Bad Request"))))
