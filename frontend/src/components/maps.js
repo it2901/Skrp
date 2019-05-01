4f1a1181d8664b262198ef127f24d4cd6b58b05e
emulberg <emulberg@stud.ntnu.no>
2019-05-01 15:02:46 +0200

    updated the map and nodegraph to match the new mocker endpoints

--- frontend/src/components/maps.js
+++ frontend/src/components/maps.js
@@ -5,100 +5,106 @@ import Control from 'react-leaflet-control'
 import { Button } from 'semantic-ui-react'
 
 export default class Maps extends Component {
-  constructor () {
-    super()
-    this.state = {
-      updaters: [],
-      liveUpdate: true,
-      liveUpdater: 0,
-      heigth: '1080px',
-      lat: 52.5,
-      lng: 13.3,
-      zoom: 12,
-      links: [{
-        cost: 123,
-        target: '123.123.123.11',
-        source: '123.123.123.12'
-      }],
-      nodes: {
-        '123.123.123.11': { pos: [52.3, 13.5], neighbours: [] },
-        '123.123.123.12': { pos: [52.2, 13.4], neighbours: [] }
-      }
-    }
-  }
-
-  change () {
-    let liveUpdater = this.state.liveUpdater
-    if (this.state.liveUpdate) {
-      liveUpdater = setInterval(() => {
-        this.setInitalState()
-      }, 5000)
-      this.setState({
-        liveUpdater: liveUpdater
-      })
-    } else {
-      clearInterval(this.state.liveUpdater)
-      this.setState({
-        liveUpdater: 0
-      })
-    }
-  }
-
-  async setInitalState () {
-    let stateToBe = await fetch('http://localhost:3001/mapnod').then(response => {
-      return response.json()
-    }).catch(err => console.error(err))
-    let nodes = stateToBe['nodes'].map(node => { return node['id'] })
-    let links = stateToBe['links']
-    let locs = stateToBe['Locations'].map(l => {
-      let pos = l['Location']['Position']
-      let lng = pos['Longitude']
-      let lat = pos['Latitude']
-      return [lat, lng]
-    })
-    let x = {}
-    for (let i = 0; i < 51; i++) {
-      x[nodes[i]] = { pos: locs[i], neighbours: [] }
-    }
-    this.setState({
-      nodes: x
-    })
-    this.setState({
-      links: links.map(link => {
-        return {
-          cost: link['cost'],
-          source: link['source'],
-          target: link['target']
+    constructor() {
+        super()
+        this.state = {
+          updaters: [],
+          liveUpdate: true,
+          liveUpdater: 0,
+            heigth: "1080px",
+        lat: 52.5,
+        lng: 13.3,
+        zoom: 12,
+        links:[{
+            cost:123,
+            target:"123.123.123.11",
+            source:"123.123.123.12"
+        }],
+        nodes:{
+          "123.123.123.11" : {pos:[52.3,13.5], neighbours:[]},
+          "123.123.123.12" : {pos:[52.2,13.4], neighbours:[]}
         }
-      })
-    })
-    this.addNeighbours()
-  }
+    }}
 
-  addNeighbours () {
-    const newState = this.state
-    let links = this.state.links
-    let nodes = this.state.nodes
-    links.forEach(link => {
-      let src = link['source']
-      let trg = link['target']
-      nodes[src]['neighbours'].push(trg)
-      nodes[trg]['neighbours'].push(src)
-    })
-    newState.nodes = nodes
-    this.setState(newState)
-  }
-
-  findLatLng (id) {
-    if (id in this.state.nodes) {
-      return this.state.nodes[id]['pos']
+    change () {
+      let liveUpdater = this.state.liveUpdater
+        if (this.state.liveUpdate){
+            liveUpdater = setInterval(() => {
+            this.setInitalState()
+          }, 5000);
+         this.setState({
+           liveUpdater:liveUpdater
+         })
+        }
+        else{
+          clearInterval(this.state.liveUpdater)
+          this.setState({
+            liveUpdater:0
+          })
+        }
     }
-  }
 
-  updateDimensions () {
-    const height = window.innerWidth >= 992 ? window.innerHeight : 400
-    this.setState({ height: height })
-  }
+    async setInitalState () {
+        let stateToBe= await fetch("http://localhost:3001/mapnod")
+        .then(response => response.json())
+        .then(data => data.collection[0])
+        .catch(err => console.error(err))
+        let nodes = stateToBe["nodes"].map(node => {return node["id"]})
+        let links = stateToBe["links"]
+        let locs = stateToBe["Locations"].map(l => {
+          let pos = l["Location"]["Position"]
+          let lng = pos["Longitude"] 
+          let lat = pos["Latitude"]
+          return [lat,lng]
+        })
+        let x = {}
+        for (let i = 0; i < 51; i++) {
+          x[nodes[i]] = {pos:locs[i],neighbours:[]}
+        }
+        this.setState({
+            nodes:x
+        })
+       this.setState({
+            links:links.map(link =>{
+                return{
+                cost:link["cost"],
+                source:link["source"],
+                target:link["target"]
+                }
+
+
+            })
+        })
+        this.addNeighbours()
+      }
+
+      addNeighbours(){
+        const newState = this.state
+        let links = this.state.links
+        let nodes = this.state.nodes
+        links.forEach(link => {
+          let src = link["source"]
+          let trg = link["target"]
+          nodes[src]["neighbours"].push(trg)
+          nodes[trg]["neighbours"].push(src)
+        })
+        newState.nodes = nodes
+        this.setState(newState)
+        
+
+      }
+
+      findLatLng(id){
+        if (id in this.state.nodes){
+          return this.state.nodes[id]["pos"]
+        }
+      }
+
+    updateDimensions() {
+        const height = window.innerWidth >= 992 ? window.innerHeight : 400
+        this.setState({ height: height })
+      }
+  
 
   componentDidMount () {
     if (!this.state.liveUpdate) {
