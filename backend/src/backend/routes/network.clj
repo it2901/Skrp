7b667b47e1aaa3ab44e3dd6b61c311073dc29036
skykanin <3789764+skykanin@users.noreply.github.com>
2019-05-02 22:54:13 +0200

    Added GET route for cleaning graph and geoloc data

--- backend/src/backend/routes/network.clj
+++ backend/src/backend/routes/network.clj
@@ -29,29 +29,65 @@
 (defn- filter-nodes
   "Filters the nodes based on ip addresses in geolocations"
   [origin-ips nodes]
-  (filter (fn [node] (some #(= :id node %) origin-ips)) nodes))
+  (filter (fn [node] (some #(= (:id node) %) origin-ips)) nodes))
 
 (defn- update-nodes
   "Updates the key node in the NetworkGraph map"
   [origin-ips networkgraph]
-  (update networkgraph :nodes filter-nodes origin-ips))
+  (update networkgraph :nodes (partial filter-nodes origin-ips)))
+
+(defn- filter-links
+  "Filters the links based on the node ips"
+  [links nodes]
+  (filter
+   (fn [link]
+     (and (some #(= (:source link) (:id %)) nodes)
+          (some #(= (:target link) (:id %)) nodes)))
+   links))
+
+(defn- update-links
+  "Updates the links based on the nodes"
+  [{:keys [nodes] :as networkgraph}]
+  (update networkgraph :links filter-links nodes))
 
 (defn- remove-nodes
-  "Removes all nodes in the NetworkGraphs which
-  don't contain corresponding geolocations"
-  [network]
-  (let [coll (:collection network)
-        geofilter (coll-filter "GeoLocation")
+  "Removes all nodes in the NetworkGraphs which don't contain
+  corresponding geolocations"
+  [coll]
+  (let [geofilter (coll-filter "GeoLocation")
         graphfilter (coll-filter "NetworkGraph")
         geocoll (:collection (first (filter geofilter coll)))
         graphnetcoll (first (filter graphfilter coll))
         ;;care with this capital O in keyword
         origin-ips (map :Originator geocoll)]
-    (update graphnetcoll :collection map (partial update-nodes origin-ips))))
+    (update graphnetcoll :collection
+            (partial map (partial update-nodes origin-ips)))))
+
+(defn- remove-links
+  "Removes all links that don't have bothcorresponding nodes"
+  [netcollgraph]
+  (update netcollgraph :collection (partial map update-links)))
+
+(defn- update-netcol
+  "Updates the root NetworkCollection"
+  [{coll :collection :as root-netcoll}]
+  (-> root-netcoll
+      (update :collection #(remove-links (remove-nodes %)))
+      ;; Quick and dirty, should make removal functions non lossy
+      (assoc-in [:collection 1] (first (filter (coll-filter "GeoLocation") coll)))))
 
 (defn network-handler
+  "Handles HTTP GET requests to the api and returns the
+  cleaned NetworkCollection json objects"
   [req]
-  (let [network (get-network-collection :latest)]
-    {:status 200
-     :headers {"Content Type" "application/json"}
-     :body "hello world"}))
+  (let [[{data :collection}] (get-network-collection :latest)
+        res (try (update-netcol (clojure.walk/keywordize-keys data))
+                 (catch Exception e
+                   (str "caught exception: " (.getMessage e))))]
+    (if-not (empty? data)
+      (if (map? res)
+        {:status 200
+         :headers {"Content Type" "application/json"}
+         :body res}
+        (error-handler-rep 500 res))
+      (error-handler-rep 404 "NetworkCollection table is empty"))))
