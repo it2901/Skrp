38c522a34e7cf79ec96b8bce6e435559476f4091
emulberg <emulberg@stud.ntnu.no>
2019-05-06 11:32:38 +0200

    updated the way tweakinput takes in data from the API

--- frontend/src/components/tweakInput.js
+++ frontend/src/components/tweakInput.js
@@ -40,8 +40,9 @@ class TweakInput extends Component {
 
   async setInitalState () {
     const stateToBe = await fetch('http://localhost:8090/configure').then(data => { return data.json() }).catch(err => console.error(err))
-    let parameters = stateToBe[0]['config']
-    let deviceId = stateToBe[0]['device_id']
+    let config = stateToBe.body['config']
+    let parameters = config['config']
+    let deviceId = config['device_id']
     // this is not allowed, plz fix me.
     // This might trigger alot of state updates
     this.device_id = deviceId
