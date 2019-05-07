f5c4159cf280f343b3ad796af195aeabd4838220
Joakim Fremstad <joa.fre@gmail.com>
2019-05-07 08:18:39 +0200

    Fix datadump

--- database/datadump.sql
+++ database/datadump.sql
@@ -87,6 +87,15 @@ COPY public.net_json_graph (net_json_graph_id, content, created) FROM stdin;
 
 
 --
+-- Data for Name: network_collection; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.network_collection (collection_id, collection, created) FROM stdin;
+1	{"type": "NetworkCollection", "collection": [{"type": "NetworkCollection", "collection": [{"type": "NetworkGraph", "label": "RuDi Network-7", "links": [{"cost": 6, "source": "192.178.1.100", "target": "192.177.1.100", "cost_text": "1020 bit/s", "properties": {}}, {"cost": 6, "source": "192.177.1.100", "target": "192.178.1.100", "cost_text": "1020 bit/s", "properties": {}}, {"cost": 6, "source": "192.177.1.100", "target": "192.166.1.100", "cost_text": "1020 bit/s", "properties": {}}, {"cost": 6, "source": "192.166.1.100", "target": "192.177.1.100", "cost_text": "1020 bit/s", "properties": {}}, {"cost": 6, "source": "192.175.1.100", "target": "192.166.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.166.1.100", "target": "192.175.1.100", "cost_text": "1020 bit/s", "properties": {}}, {"cost": 6, "source": "192.175.1.100", "target": "192.178.1.100", "cost_text": "1020 bit/s", "properties": {}}, {"cost": 6, "source": "192.178.1.100", "target": "192.175.1.100", "cost_text": "1020 bit/s", "properties": {}}], "nodes": [{"id": "192.178.1.100", "label": "NODE-7A"}, {"id": "192.177.1.100", "label": "NODE-7B"}, {"id": "192.166.1.100", "label": "NODE-C"}, {"id": "192.175.1.100", "label": "NODE-7D"}], "metric": "rudi", "version": "1.0.7", "protocol": "olsrv2", "revision": "rudi7", "router_id": "192.178.1.100", "topology_id": "topology_rudi"}, {"type": "NetworkGraph", "label": "RuDi simulated Network", "links": [{"cost": 6, "source": "192.168.1.100", "target": "192.167.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.167.1.100", "target": "192.168.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.167.1.100", "target": "192.166.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.166.1.100", "target": "192.167.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.165.1.100", "target": "192.166.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.166.1.100", "target": "192.165.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.165.1.100", "target": "192.168.1.100", "cost_text": "1020 bit/s"}, {"cost": 6, "source": "192.168.1.100", "target": "192.165.1.100", "cost_text": "1020 bit/s"}], "nodes": [{"id": "192.168.1.100", "label": "NODE-A"}, {"id": "192.167.1.100", "label": "NODE-B"}, {"id": "192.166.1.100", "label": "NODE-C"}, {"id": "192.165.1.100", "label": "NODE-D"}], "metric": "rudi", "version": "1.0.1", "protocol": "olsrv2", "revision": "rudi2080212", "router_id": "192.168.1.100", "topology_id": "topology_test"}]}, {"type": "NetworkCollection", "collection": [{"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.1114158424862665697219199500977993011474609375, "Longitude": 18.0265903472900390625}, "Originator": "192.178.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.12357303344450798476827912963926792144775390625, "Longitude": 18.00693511962890625}, "Originator": "192.177.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.12112641752724329080592724494636058807373046875, "Longitude": 17.9791259765625}, "Originator": "192.166.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.11437818671146970928020891733467578887939453125, "Longitude": 18.00294399261474609375}, "Originator": "192.175.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.13500589733093448785439250059425830841064453125, "Longitude": 17.96329021453857421875}, "Originator": "192.168.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.138224060321334718537400476634502410888671875, "Longitude": 17.977237701416015625}, "Originator": "192.167.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.11785546082753484142813249491155147552490234375, "Longitude": 17.9357814788818359375}, "Originator": "192.165.1.100"}, {"Time": {"Day": 2, "HourTime": 8, "MinuteTime": 5, "SecondTime": 44, "Year4Digit": 119, "MonthNumeric": 3}, "type": "GeoLocation", "Position": {"Latitude": 53.1217960315141368710101232863962650299072265625, "Longitude": 18.08740139007568359375}, "Originator": "192.174.1.100"}]}]}	2019-05-07 05:07:31.31862+00
+\.
+
+
+--
 -- Data for Name: position_log; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -105,6 +114,30 @@ COPY public.system_log (system_log_id, device_id, adaption_id, description, crea
 4	1	\N	test	2019-04-22 19:37:05.657806+00
 5	1	\N	test	2019-04-29 18:33:49.731702+00
 6	1	1	test	2019-04-29 18:33:55.283602+00
+7	2	3	done	2019-05-07 05:41:24.386817+00
+8	2	4	done	2019-05-07 05:41:37.239556+00
+9	2	5	done	2019-05-07 05:41:40.792198+00
+10	2	6	done	2019-05-07 05:41:53.948331+00
+11	2	7	done	2019-05-07 05:44:05.325588+00
+12	2	8	done	2019-05-07 05:44:05.328172+00
+13	2	9	done	2019-05-07 05:44:05.330104+00
+14	2	10	done	2019-05-07 05:44:05.332207+00
+15	2	11	done	2019-05-07 05:44:05.334705+00
+16	2	12	done	2019-05-07 05:44:05.336701+00
+17	2	13	done	2019-05-07 05:44:05.338877+00
+18	2	14	done	2019-05-07 05:44:05.340726+00
+19	2	15	done	2019-05-07 05:44:05.348108+00
+20	2	16	done	2019-05-07 05:44:05.355658+00
+21	2	17	done	2019-05-07 05:44:05.363105+00
+22	2	18	done	2019-05-07 05:44:05.371172+00
+23	2	19	done	2019-05-07 05:44:05.373484+00
+24	2	20	done	2019-05-07 05:44:05.374633+00
+25	2	21	done	2019-05-07 05:44:05.377087+00
+26	2	22	done	2019-05-07 05:44:05.379308+00
+27	2	23	done	2019-05-07 05:44:05.38124+00
+28	2	24	done	2019-05-07 05:44:05.383915+00
+29	2	25	done	2019-05-07 05:44:05.386018+00
+30	2	26	done	2019-05-07 05:44:05.387959+00
 \.
 
 
@@ -137,12 +170,18 @@ SELECT pg_catalog.setval('public.net_json_graph_net_json_graph_id_seq', 1, false
 
 
 --
+-- Name: network_collection_collection_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.network_collection_collection_id_seq', 1, true);
+
+
+--
 -- Name: system_log_system_log_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.system_log_system_log_id_seq', 6, true);
 
-insert into public.network_collection (collection) values('{"type":"NetworkCollection","collection":[{"type":"NetworkCollection","collection":[{"type":"NetworkGraph","protocol":"olsrv2","version":"1.0.7","revision":"rudi7","metric":"rudi","label":"RuDi Network-7","nodes":[{"id":"192.178.1.100","label":"NODE-7A"},{"id":"192.177.1.100","label":"NODE-7B"},{"id":"192.166.1.100","label":"NODE-C"},{"id":"192.175.1.100","label":"NODE-7D"}],"links":[{"source":"192.178.1.100","target":"192.177.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"},{"source":"192.177.1.100","target":"192.178.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"},{"source":"192.177.1.100","target":"192.166.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"},{"source":"192.166.1.100","target":"192.177.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"},{"source":"192.175.1.100","target":"192.166.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.166.1.100","target":"192.175.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"},{"source":"192.175.1.100","target":"192.178.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"},{"source":"192.178.1.100","target":"192.175.1.100","cost":6,"properties":{},"cost_text":"1020 bit/s"}],"router_id":"192.178.1.100","topology_id":"topology_rudi"},{"type":"NetworkGraph","protocol":"olsrv2","version":"1.0.1","revision":"rudi2080212","metric":"rudi","label":"RuDi simulated Network","nodes":[{"id":"192.168.1.100","label":"NODE-A"},{"id":"192.167.1.100","label":"NODE-B"},{"id":"192.166.1.100","label":"NODE-C"},{"id":"192.165.1.100","label":"NODE-D"}],"links":[{"source":"192.168.1.100","target":"192.167.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.167.1.100","target":"192.168.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.167.1.100","target":"192.166.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.166.1.100","target":"192.167.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.165.1.100","target":"192.166.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.166.1.100","target":"192.165.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.165.1.100","target":"192.168.1.100","cost":6,"cost_text":"1020 bit/s"},{"source":"192.168.1.100","target":"192.165.1.100","cost":6,"cost_text":"1020 bit/s"}],"router_id":"192.168.1.100","topology_id":"topology_test"}]},{"type":"NetworkCollection","collection":[{"type":"GeoLocation","Originator":"192.178.1.100","Position":{"Latitude":53.1114158424862665697219199500977993011474609375,"Longitude":18.0265903472900390625},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.177.1.100","Position":{"Latitude":53.12357303344450798476827912963926792144775390625,"Longitude":18.00693511962890625},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.166.1.100","Position":{"Latitude":53.12112641752724329080592724494636058807373046875,"Longitude":17.9791259765625},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.175.1.100","Position":{"Latitude":53.11437818671146970928020891733467578887939453125,"Longitude":18.00294399261474609375},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.168.1.100","Position":{"Latitude":53.13500589733093448785439250059425830841064453125,"Longitude":17.96329021453857421875},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.167.1.100","Position":{"Latitude":53.138224060321334718537400476634502410888671875,"Longitude":17.977237701416015625},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.165.1.100","Position":{"Latitude":53.11785546082753484142813249491155147552490234375,"Longitude":17.9357814788818359375},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}},{"type":"GeoLocation","Originator":"192.174.1.100","Position":{"Latitude":53.1217960315141368710101232863962650299072265625,"Longitude":18.08740139007568359375},"Time":{"Year4Digit":119,"MonthNumeric":3,"Day":2,"HourTime":8,"MinuteTime":5,"SecondTime":44}}]}]}');
 
 --
 -- PostgreSQL database dump complete
